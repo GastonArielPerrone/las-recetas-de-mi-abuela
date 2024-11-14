f82d0072252213dfe936673cab99086b00f683e0
Gaston Ariel Perrone <gastonarielp1812@gmail.com>
2024-11-14 12:16:01 -0300

    files are remved.

--- render.js
+++
@@ -1,4 +0,0 @@
-fetch('https://las-recetas-de-mi-abuela.onrender.com/api/data')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
