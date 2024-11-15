78bc6d134c74133246246dc31f759d8bc5636422
Gaston Ariel Perrone <gastonarielp1812@gmail.com>
2024-11-15 10:55:16 -0300

    update host and port.

--- consulta_receta.py
+++ consulta_receta.py
@@ -55,4 +55,4 @@ def obtener_recetas_por_categoria(categoria):
         return jsonify({'recetas': []})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
