23aff0d3e2aa2ddb18dfe6df32fa19a01550f659
Gastón Ariel Perrone <101432889+GastonArielPerrone@users.noreply.github.com>
2024-12-04 02:32:44 -0300

    Actualizacion modelo_orm.py

--- modelo_orm.py
+++ modelo_orm.py
@@ -53,7 +53,7 @@ def inicializar_bd():
     db.close()
 
 # Ruta para manejar la carga de recetas
-@app.route('/carga_de_receta', methods=['POST', 'GET'])
+@app.route('/Carga_de_receta.html', methods=['GET','POST'])
 def carga_receta():
     try:
         # Obtener datos del formulario
