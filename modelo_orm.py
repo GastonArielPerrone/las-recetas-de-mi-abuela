b85912dab92e1c51ea28a68e0500f92cc2150a43
Gaston Ariel Perrone <gastonarielp1812@gmail.com>
2024-12-04 12:46:59 -0300

    route in line 63 is modified.

--- modelo_orm.py
+++ modelo_orm.py
@@ -60,7 +60,7 @@ def inicializar_bd():
     db.close()
 
 # Ruta para manejar la carga de recetas
-@app.route('/carga_receta', methods=['GET','POST'])
+@app.route('/Carga_de_receta.html', methods=['GET','POST'])
 def carga_receta():
     try:
         # Obtener datos del formulario
