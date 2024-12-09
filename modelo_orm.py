2f93242a635d61693d629561bd25431622fd25a8
Gaston Ariel Perrone <gastonarielp1812@gmail.com>
2024-12-09 11:50:54 -0300

    port is edited.

--- modelo_orm.py
+++ modelo_orm.py
@@ -127,4 +127,4 @@ if __name__ == '__main__':
     print("Rutas registradas:")
     print(app.url_map)
     inicializar_bd()
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
